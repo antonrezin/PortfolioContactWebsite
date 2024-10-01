d2073171d1cf2d4d38020cfad4178c0306f019be
Anton Rezin <rezin.anton@gmail.com>
2024-10-02 00:23:28 +0300

    commit

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -13,26 +13,29 @@ function Home() {
       <Typography
         fontFamily={"monospace"}
         className="homeDesc"
-        variant="h5"
         textAlign={"center"}
       >
-        Hi, I'm Anton Rezin!
-        <br /> <br />
-        I’m a <b>Junior Full Stack Developer</b> from the
-        <br />
-        <b>Helsinki Metropolitan Area</b>
-        <br />
-        with a passion for creating seamless digital experiences.
-        <br />
-        Whether it’s building intuitive user interfaces or developing robust
-        backend systems, I enjoy bringing ideas to life through clean, efficient
-        code.
-        <br />
-        Currently finishing my degree in Full Stack Development, I’m eager to
-        apply my skills in real-world projects and collaborate with teams that
-        value innovation and quality.
-        <br /> <br />
-        Let’s create something extraordinary together!
+        <b>
+          Hi, I'm Anton Rezin!
+          <br /> <br />
+          I’m a Junior Full Stack Developer from the
+          <br />
+          Helsinki Metropolitan Area
+          <br />
+          with a passion for creating seamless digital experiences.
+          <br />
+          Whether it’s building intuitive user interfaces or developing robust
+          backend systems, I enjoy bringing ideas to life through clean,
+          efficient code. My expertise spans both backend and frontend
+          development, allowing me to create well-rounded applications that
+          deliver exceptional user experiences.
+          <br />
+          Currently finishing my degree in Full Stack Development, I’m eager to
+          apply my skills in real-world projects and collaborate with teams that
+          value innovation and quality.
+          <br /> <br />
+          Let’s create something extraordinary together!
+        </b>
       </Typography>
     </Stack>
   );
