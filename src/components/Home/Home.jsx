5e6bf059829a0e06ba11814a7c28af228d217e3d
Anton Rezin <rezin.anton@gmail.com>
2024-10-02 11:27:47 +0300

    commit

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -7,6 +7,7 @@ function Home() {
     <Stack className="homePage">
       <Typography
         fontFamily={"monospace"}
+        className="homeDesc"
         variant="h3"
         sx={{ fontSize: { xs: "1.5rem", md: "2rem" } }}
       >
@@ -21,28 +22,34 @@ function Home() {
         fontFamily={"monospace"}
         className="homeDesc"
         textAlign={"center"}
+        variant="h6"
       >
-        <b>
-          Hi, I'm Anton Rezin!
-          <br /> <br />
-          I’m a Junior Full Stack Developer from the
-          <br />
-          Helsinki Metropolitan Area
-          <br />
-          with a passion for creating seamless digital experiences.
-          <br />
-          Whether it’s building intuitive user interfaces or developing robust
-          backend systems, I enjoy bringing ideas to life through clean,
-          efficient code. My expertise spans both backend and frontend
-          development, allowing me to create well-rounded applications that
-          deliver exceptional user experiences.
-          <br />
-          Currently finishing my degree in Full Stack Development, I’m eager to
-          apply my skills in real-world projects and collaborate with teams that
-          value innovation and quality.
-          <br /> <br />
-          Let’s create something extraordinary together!
-        </b>
+        Hi, I'm Anton Rezin!
+      </Typography>
+      <Typography
+        fontFamily={"monospace"}
+        className="homeDesc"
+        textAlign={"center"}
+        variant="h6"
+      >
+        I’m a Junior Full Stack Developer from the Helsinki Metropolitan Area
+        with a passion for creating seamless digital experiences. Whether it’s
+        building intuitive user interfaces or developing robust backend systems,
+        I enjoy bringing ideas to life through clean, efficient code. My
+        expertise spans both backend and frontend development, allowing me to
+        create well-rounded applications that deliver exceptional user
+        experiences. Currently finishing my degree in Full Stack Development,
+        I’m eager to apply my skills in real-world projects and collaborate with
+        teams that value innovation and quality.
+      </Typography>
+      <Typography
+        fontFamily={"monospace"}
+        className="homeDesc"
+        textAlign={"center"}
+        variant="h6"
+      >
+        {" "}
+        Let’s create something extraordinary together!
       </Typography>
     </Stack>
   );
