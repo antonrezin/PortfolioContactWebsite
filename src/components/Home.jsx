c071b68d582b8d6baee2fe155e5512914e5858fb
Anton Rezin <rezin.anton@gmail.com>
2024-12-19 22:21:16 +0200

    commit.

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,4 +1,4 @@
-import { Stack, Box, Typography } from "@mui/material";
+import { Stack, Box, Typography, List, ListItem, Link } from "@mui/material";
 import profilePicture from "/assets/ProfilePicture.jpg";
 import LinkedInIcon from "@mui/icons-material/LinkedIn";
 import GitHubIcon from "@mui/icons-material/GitHub";
@@ -59,7 +59,7 @@ function Home() {
         variant="h4"
         fontWeight={"bold"}
         textAlign={"center"}
-        maxWidth={"1000px"}
+        maxWidth={"800px"}
       >
         Hi, I’m Anton Rezin!
       </Typography>
@@ -69,38 +69,126 @@ function Home() {
         variant="h5"
         fontWeight={"bold"}
         textAlign={"center"}
-        maxWidth={"1000px"}
+        maxWidth={"800px"}
       >
-        I’m an aspiring Full Stack Developer from the Helsinki Metropolitan
-        Area, passionate about crafting seamless digital experiences. I
-        specialize in building intuitive user interfaces and robust backend
-        systems, focusing on scalable and efficient solutions.
+        I’m an aspiring Full Stack Developer based in the Helsinki Metropolitan
+        Area, passionate about crafting seamless digital experiences. My current
+        role as a Full Stack Developer Intern at Innate AI through Herizon has
+        further solidified my dedication to software development. I enjoy
+        working in innovative and dynamic environments where I can contribute
+        and grow professionally.
         <br />
         <br />
-        At Haaga-Helia University of Applied Sciences, I’ve built personal
-        software development projects and completed three team-based agile
-        software development projects, where I gained hands-on experience with
-        Java Spring Boot, REST APIs, React, Vite, and cutting-edge AI
-        technologies. In one project, my team developed an AI-powered Image
-        Recognition Application under contract with an organization, leveraging
-        Gemini AI, ChatGPT API, Node.js, Express.js, TypeScript, React, and
-        Vite. A certification for my participation and work on this project is
-        forthcoming.
+        Over the course of my studies at Haaga-Helia University of Applied
+        Sciences and through hands-on project experience, I have developed
+        comprehensive skills in both front-end and back-end technologies. My
+        programming skills include HTML, CSS, JavaScript, Java, Python, and
+        TypeScript. Additionally, I have experience with various other
+        technologies such as React + Vite, Spring Boot, Node.js, SQL, Docker,
+        RESTful APIs, Jest, JUnit, Git, GitHub Actions, and Amazon Web Services
+        (AWS).
         <br />
         <br />
-        I’m currently a Full Stack Developer Intern at Innate AI through
-        Herizon, completing a 10–12 week internship. Having finished my
-        coursework, I am now focused on my internship and thesis, and I will be
-        open to full-time Full Stack Developer opportunities starting March 10th.
       </Typography>
-      <br />
+      <Typography
+        fontFamily={"monospace"}
+        variant="h5"
+        fontWeight={"bold"}
+        textAlign={"center"}
+        maxWidth={"800px"}
+      >
+        Some of the notable projects I have worked on include:
+      </Typography>
+      <List>
+        <Typography
+          fontFamily={"monospace"}
+          variant="h5"
+          fontWeight={"bold"}
+          textAlign={"center"}
+          maxWidth={"800px"}
+        >
+          <Link
+            href="https://github.com/orgs/EkaTiimi/repositories"
+            target="_blank"
+            rel="noopener noreferrer"
+            sx={{ textDecoration: "none", color: "primary.main" }}
+          >
+            Query Service
+          </Link>
+          <br />
+          Developed using Java Spring Boot, React, Vite, REST, and Material-UI.
+        </Typography>
+        <br />
+        <Typography
+          fontFamily={"monospace"}
+          variant="h5"
+          fontWeight={"bold"}
+          textAlign={"center"}
+          maxWidth={"800px"}
+        >
+          <Link
+            href="https://github.com/TeamRed-Ohjelmistoprojekti2/TimeManagement"
+            target="_blank"
+            rel="noopener noreferrer"
+            sx={{ textDecoration: "none", color: "primary.main" }}
+          >
+            Time Management Application
+          </Link>
+          <br />
+          Built with Java Spring Boot, JWT, React, Vite, REST, and Material-UI.
+        </Typography>
+        <br />
+        <Typography
+          fontFamily={"monospace"}
+          variant="h5"
+          fontWeight={"bold"}
+          textAlign={"center"}
+          maxWidth={"800px"}
+        >
+          <Link
+            href="https://kalustebottifrontend-arvolaskuri-demo.2.rahtiapp.fi/"
+            target="_blank"
+            rel="noopener noreferrer"
+            sx={{ textDecoration: "none", color: "primary.main" }}
+          >
+            Image Recognition Application
+          </Link>
+          <br />
+          Created with Node.js, Express.js, React, Vite, REST, TypeScript, Gemini AI,
+          ChatGPT API, and Material-UI.
+        </Typography>
+      </List>
+      <Typography
+        fontFamily={"monospace"}
+        variant="h5"
+        fontWeight={"bold"}
+        textAlign={"center"}
+        maxWidth={"800px"}
+      >
+        <br />
+        These projects have equipped me with practical experience in version
+        control, API design, and agile methodologies, ensuring that I can
+        deliver value quickly in real-world projects.
+        <br />
+        <br />
+        My colleagues often describe me as analytical, eager to learn, and
+        solution-oriented. I am a team player who brings positive energy and
+        takes responsibility in both individual and group settings. Challenges
+        are opportunities for me to grow, and this mindset has been instrumental
+        in my academic and professional success.
+        <br />
+        <br />I have completed all my coursework at Haaga-Helia University of
+        Applied Sciences and have only my thesis left to complete before
+        graduation.
+      </Typography>
       <Typography
         fontFamily={"monospace"}
         variant="h4"
         fontWeight={"bold"}
         textAlign={"center"}
-        maxWidth={"1000px"}
+        maxWidth={"1400px"}
       >
+        <br />
         Let’s create something extraordinary together!
       </Typography>
     </Stack>
