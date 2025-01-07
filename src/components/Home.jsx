25639df15d59f78dbc0c98788f43965b5aae3053
Anton Rezin <rezin.anton@gmail.com>
2025-01-07 03:03:14 +0200

    Update Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -44,7 +44,7 @@ function Home() {
         Anton Rezin
       </Typography>
 
-      <hr style={{ borderColor: "#3b3b3b", width: "500px" }} />
+      <hr style={{ borderColor: "#000000", width: "500px" }} />
 
       <Stack direction="row" spacing={3}>
         <Link
@@ -63,7 +63,7 @@ function Home() {
         </Link>
       </Stack>
 
-      <hr style={{ borderColor: "#3b3b3b", width: "500px" }} />
+      <hr style={{ borderColor: "#000000", width: "500px" }} />
 
 
       <Typography
@@ -89,7 +89,7 @@ function Home() {
 
         <Typography
           fontFamily={"monospace"}
-          variant="h5"
+          variant="h6"
           textAlign={"center"}
           maxWidth={"800px"}
         >
